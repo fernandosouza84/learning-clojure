03a3d32fb943513dded444e39b642242fee03e22
Fernando Souza <fernando.souza@truckpad.com.br>
2020-06-19 19:02:14 -0300

    Hello World in Clojure

---
+++ HelloWorld/hello-world.clj
@@ -0,0 +1,11 @@
+; My first contact with clojure
+
+(println "Hello World")
+
+(println "Hellooooo Heloooo" "Hola")
+
+; Defining and using variables
+
+(def textString "My first contact with clojure")
+
+(println "Str -> " textString)
